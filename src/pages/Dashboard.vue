4073d6734b68f6f15e1b679b0f3b98a3620aafd2
soyeon <ssafy.soyeon@gmail.com>
2021-05-26 10:03:45 +0900

    clear dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -11,50 +11,8 @@
       <apt-card v-for="(apt, index) in apts" :key="index" :apt="apt" />
     </div>
     <!-- Apart -->
-    <div class="row">
-      <div
-        class="col-md-6 col-xl-3"
-        v-for="stats in statsCards"
-        :key="stats.title"
-      >
-        <stats-card>
-          <div
-            class="icon-big text-center"
-            :class="`icon-${stats.type}`"
-            slot="header"
-          >
-            <i class="ti-home"></i>
-          </div>
-          <div class="numbers" slot="content">
-            <p>{{ stats.title }}</p>
-            {{ stats.value }}
-          </div>
-          <div class="stats" slot="footer">
-            <i :class="stats.footerIcon"></i> {{ stats.footerText }}
-          </div>
-        </stats-card>
-      </div>
-    </div>
-    <!--Charts-->
-    <div class="row">
-      <div class="col-12">
-        <chart-card
-          title="Users behavior"
-          sub-title="24 Hours performance"
-          :chart-data="usersChart.data"
-          :chart-options="usersChart.options"
-        >
-          <span slot="footer">
-            <i class="ti-reload"></i> Updated 3 minutes ago
-          </span>
-          <div slot="legend">
-            <i class="fa fa-circle text-info"></i> Open
-            <i class="fa fa-circle text-danger"></i> Click
-            <i class="fa fa-circle text-warning"></i> Click Second Time
-          </div>
-        </chart-card>
-      </div>
 
+    <div class="row">
       <div class="col-md-6 col-12">
         <news-card
           title="실시간 뉴스 정보"
@@ -63,40 +21,6 @@
         </news-card>
       </div>
 
-      <div class="col-md-6 col-12">
-        <chart-card
-          title="Email Statistics"
-          sub-title="Last campaign performance"
-          :chart-data="preferencesChart.data"
-          chart-type="Pie"
-        >
-          <span slot="footer">
-            <i class="ti-timer"></i> Campaign set 2 days ago</span
-          >
-          <div slot="legend">
-            <i class="fa fa-circle text-info"></i> Open
-            <i class="fa fa-circle text-danger"></i> Bounce
-            <i class="fa fa-circle text-warning"></i> Unsubscribe
-          </div>
-        </chart-card>
-      </div>
-
-      <div class="col-md-6 col-12">
-        <chart-card
-          title="2015 Sales"
-          sub-title="All products including Taxes"
-          :chart-data="activityChart.data"
-          :chart-options="activityChart.options"
-        >
-          <span slot="footer">
-            <i class="ti-check"></i> Data information certified
-          </span>
-          <div slot="legend">
-            <i class="fa fa-circle text-info"></i> Tesla Model S
-            <i class="fa fa-circle text-warning"></i> BMW 5 Series
-          </div>
-        </chart-card>
-      </div>
       <!--공지사항-->
       <div class="col-md-6 col-12">
         <notice-card title="공지사항" sub-title="공지사항을 확인해주세요!">
